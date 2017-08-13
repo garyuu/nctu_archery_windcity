0949b4ee399aeec5965000f5f41e2a99f1ce2bac
周咸良 <hls.chou@gmail.com>
2017-08-13 20:32:13 +0800

    Add ResultPage

--- js/index.js
+++ js/index.js
@@ -1,11 +1,40 @@
 $("#verifyPage").hide();
+$("#resultPage").hide();
 //$("#scoringEnabled").hide();
 //$("#inputError").hide();
+
 $("#btnStartScoring").click(function(){
   $("#mainPage").hide();
   $("#verifyPage").show();
 });
-$("#btnBackToMainPage").click(function(){
+
+$("#btnBackToMainPage1").click(function(){
   $("#verifyPage").hide();
   $("#mainPage").show();
+});
+
+$("#btnLookUpResult").click(function(){
+  $("#mainPage").hide();
+  $("#resultPage").show();
+});
+
+$("#btnBackToMainPage2").click(function(){
+  $("#resultPage").hide();
+  $("#mainPage").show();
+});
+
+$('#contestTabs').on('click', 'a[data-toggle="tab"]', function(e) {
+  e.preventDefault();
+
+  var $link = $(this);
+
+  if (!$link.parent().hasClass('active')) {
+
+    $('.tab-content:not(.' + $link.attr('href').replace('#','') + ') .tab-pane').removeClass('active');
+
+    $('a[href="' + $link.attr('href') + '_all"][data-toggle="tab"]').click();
+
+    $('.tab-content.' + $link.attr('href').replace('#','') + ' .tab-pane:first').addClass('active');
+  }
+
 });
