61b759d9179a6e4d59c923da32ba27140ec8d035
周咸良 <hls.chou@gmail.com>
2017-09-26 15:20:20 +0800

    Separate pages

--- js/index.js
+++ js/index.js
@@ -1,35 +1,8 @@
-$("#verifyPage").hide();
-$("#resultPage").hide();
 $("#scoringPage").hide();
-
-$("#btnStartScoring").click(function(){
-  $("#mainPage").hide();
-  $("#verifyPage").show();
-});
-
-$("#btnBackToMainPage1").click(function(){
-  $("#verifyPage").hide();
-  $("#mainPage").show();
-});
-
-$("#btnLookUpResult").click(function(){
-  $("#mainPage").hide();
-  $("#resultPage").show();
-});
-
-$("#btnBackToMainPage2").click(function(){
-  $("#resultPage").hide();
-  $("#mainPage").show();
-});
 
 $("#btnSubmitCode").click(function(){
   $("#verifyPage").hide();
   $("#scoringPage").show();
-});
-
-$("#btnBackToMainPage3").click(function(){
-  $("#scoringPage").hide();
-  $("#mainPage").show();
 });
 
 $("ul.nav-tabs a").click(function(e){
